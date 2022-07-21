1b02e4daa2de694ec7505c6edff344e51dd9f05d
Kasparas Zakrys <k.zakrys@gmail.com>
2022-07-21 15:53:19 +0300

    EndOfSection2

---
+++ Project/Project.API/Data/WalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.API.Models.Domain;
+
+namespace Project.API.Data
+{
+    public class WalksDbContext: DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
